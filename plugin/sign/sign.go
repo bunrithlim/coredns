eda860fef02acbb96b6247c5cafcc270b855603d
Miek Gieben <miek@miek.nl>
2019-07-01 10:04:52 +0100

    plugin/sign: a plugin that signs zones
    Sign is a plugin that signs zone data (on disk). The README.md details
    what exactly happens to should be accurate related to the code.
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * The change to file/* need to be backed out, once some other changes in
     another PR are in - these are just here to make things work now.
    
    * There are a couple of things missing, documented in TODO; these mostly
     deal with setup and tear down. But also some more important things like
     adding NSEC and not signing glue records. Also more tests need be added.

---
+++ plugin/sign/sign.go
@@ -0,0 +1,112 @@
+package sign
+
+import (
+	"os"
+	"time"
+
+	"github.com/coredns/coredns/plugin/file/tree"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
+
+	"github.com/miekg/dns"
+)
+
+// Sign holds the data need to sign a zone file.
+type Sign struct {
+	keys   []Pair
+	origin string
+
+	expiration uint32
+	inception  uint32
+	ttl        uint32
+
+	directory  string
+	dbfile     string
+	signedfile string
+}
+
+var log = clog.NewWithPlugin("sign")
+
+// Sign signs a zone file according to the parameters in s.
+func (s Sign) Sign(origin string) error {
+	now := time.Now()
+
+	rd, err := os.Open(s.dbfile)
+	if err != nil {
+		return err
+	}
+
+	z, err := parse(rd, origin, s.dbfile)
+	if err != nil {
+		return err
+	}
+
+	s.inception, s.expiration = lifetime(time.Now().UTC())
+	s.origin = origin
+
+	s.ttl = z.Apex.SOA.Header().Ttl
+	z.Apex.SOA.Serial = uint32(time.Now().Unix())
+	names := names(origin, z)
+
+	nsec := NSEC(origin, next(origin, names, 0), s.ttl, []uint16{dns.TypeSOA, dns.TypeNS}) // need to dish out correct types
+	z.Insert(nsec)
+
+	for _, pair := range s.keys {
+		z.Insert(pair.Public.ToDS(dns.SHA1))
+		z.Insert(pair.Public.ToDS(dns.SHA256))
+		z.Insert(pair.Public.ToCDNSKEY())
+	}
+	for _, pair := range s.keys {
+		rrsig, err := pair.signRRs([]dns.RR{z.Apex.SOA}, s.origin, s.ttl, s.inception, s.expiration)
+		if err != nil {
+			return err
+		}
+		z.Insert(rrsig)
+		rrsig, err = pair.signRRs(z.Apex.NS, s.origin, s.ttl, s.inception, s.expiration)
+		if err != nil {
+			return err
+		}
+		z.Insert(rrsig)
+		rrsig, err = pair.signRRs([]dns.RR{nsec}, s.origin, s.ttl, s.inception, s.expiration)
+		if err != nil {
+			return err
+		}
+		z.Insert(rrsig)
+	}
+
+	// We are walking the tree in the same direction, so names[] can be used here to indicated the next element.
+	i := 1
+	z.Tree.Do(func(e *tree.Elem) bool {
+		nsec := NSEC(e.Name(), next(origin, names, i), s.ttl, []uint16{dns.TypeSOA, dns.TypeNS}) // e.Types() or something
+		z.Insert(nsec)
+		// nsec ownername should be OK.
+
+		for _, rrs := range e.M() {
+			if rrs[0].Header().Rrtype == dns.TypeRRSIG {
+				continue
+			}
+			for _, pair := range s.keys {
+				rrsig, err := pair.signRRs(rrs, s.origin, s.ttl, s.inception, s.expiration)
+				if err != nil {
+					return true
+				}
+				e.Insert(rrsig)
+			}
+		}
+		i++
+		return false
+	})
+
+	s.write(z) // error handling, once booleans are gone
+
+	log.Infof("Signed %q with %d key(s) in %s, saved in %q", origin, len(s.keys), time.Since(now), s.signedfile)
+
+	return nil
+}
+
+func lifetime(now time.Time) (uint32, uint32) {
+	incep := uint32(now.Add(-3 * time.Hour).Unix()) // -(2+1) hours, be sure to catch daylight saving time and such
+	expir := uint32(now.Add(threeWeeks).Unix())     // sign for 21 days
+	return incep, expir
+}
+
+const threeWeeks = 3 * 7 * 24 * time.Hour
