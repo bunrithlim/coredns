7ec6a97b57c2b37495c59745e689701571618617
Miek Gieben <miek@miek.nl>
2019-07-14 21:46:57 +0100

    start some parse work
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/sign/setup.go
+++ plugin/sign/setup.go
@@ -1,1 +1,27 @@
-package sign
+package file
+
+import (
+	"github.com/coredns/coredns/plugin"
+
+	"github.com/mholt/caddy"
+)
+
+func init() {
+	caddy.RegisterPlugin("sign", caddy.Plugin{
+		ServerType: "dns",
+		Action:     setup,
+	})
+}
+
+func setup(c *caddy.Controller) error {
+	zones, err := parse(c)
+	if err != nil {
+		return plugin.Error("sign", err)
+	}
+
+	return nil
+}
+
+func parse(c *caddy.Controller) error {
+	return nil
+}
